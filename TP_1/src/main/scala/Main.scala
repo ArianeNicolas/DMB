588039fecc43a9368a13f7e2503d4a3b8440a240
ArianeNicolas <ariane.nicolas.bis@gmail.com>
2025-02-08 12:47:47 +0100

    finished TP 1 comments + began TP 2 comments

--- TP_1/src/main/scala/Main.scala
+++ TP_1/src/main/scala/Main.scala
@@ -3,6 +3,7 @@ import org.apache.spark.sql.SparkSession
 
 object Main extends App {
 
+    // Création de la session Spark
     val spark = SparkSession.builder()
         .appName("TP1_DMB")
         .master("local[*]")
@@ -10,17 +11,23 @@ object Main extends App {
 
     import spark.implicits._
 
-    // ------- Exercice 1 --------
+    // ------- Exercice 1 : Les meilleurs joueurs --------
 
     // Question 1 : Chargement du jeu de données
+
     val textFile = spark.read.textFile("agg_match_stats_0_100000.csv")
-/*
+
     // Question 2 : On garde seulement le nom du joueur et son nombre de kills
+
     val name_kills = textFile.map(line => line.split(","))
         .map(fields => (fields(10), fields(11)))
 
-    // Question 3
+    // Question 3 : Moyennes des éliminations pour un joueur
 
+    /* Création de deux rdd,
+       un pour compter le nombre de parties pour un joueur,
+       et un pour compter son nombre de kills total.
+    */
     val rdd_nbr_parties = textFile
         .map(line => line.split(","))
         .map(fields => (fields(11), 1))
@@ -30,35 +37,115 @@ object Main extends App {
         .map(fields => (fields(11), fields(10).toFloat))
         .rdd
 
-    // On récupère le nombre de partie pour un joueur
+    /* On récupère le nombre de parties pour un joueur
+        à l'aide de la fonction reduceByKey :
+        comme on avait mis un 1 pour chaque ligne dans la deuxième colonne,
+        reduceByKey nous rend la somme de tous ces 1,
+        donc le nombre total de parties.
+    */
     var nbr_parties = rdd_nbr_parties.reduceByKey(_+_).collect()
 
-    // On récupère le nombre de kills pour un joueur
+    /* On récupère le nombre de kills pour un joueur
+        Même chose que juste au dessus, sauf que cette fois au lieu des 1
+        on a le nombre de kills de la partie,
+        la somme donne donc e nombre total de kills du joueur
+    */
     var nbr_kills = rdd_nbr_kills.reduceByKey(_+_)
 
-    // On calcule la moyenne de kills pour chaque joueur
-    //var moyenne_kills = nbr_kills.map(x => (x._1, x._2 /nbr_parties.filter(y => y._1 == x._1)(0)._2), nbr_parties.filter(y => y._1 == x._1)(0)._2))
+    /* On calcule la moyenne de kills pour chaque joueur,
+        en divisant le nombre de kills par le nombre de parties
+    */
+    var moyenne_kills = nbr_kills.map(x => (x._1, x._2 /nbr_parties.filter(y => y._1 == x._1)(0)._2))
 
-    // Question 4 : On garde seulement les joueurs ayant fait le plus de kills
-    //var bests_10 = moyenne_kills.sortBy(_._2, false).take(10)
+    // Question 4 : On garde seulement les 10 joueurs ayant fait le plus de kills
+    var bests_10 = moyenne_kills.sortBy(_._2, false).take(10)
 
     // Question 5 : On garde seulement les joueurs ayant fait au moins 4 parties
     var many_games = nbr_kills.filter(fields => fields._1 != "") // Question 6 : On enlève les joueurs ayant un nom vide
-    .filter(x => nbr_parties.filter(y => y._1 == x._1)(0)._2 > 3)
+                .filter(x => nbr_parties.filter(y => y._1 == x._1)(0)._2 > 3)
 
     // Question 7 : On réaffiche à nouveau les 10 meilleurs joueurs
     var moyenne_kills_many_games = many_games.map(x => (x._1, x._2 /nbr_parties.filter(y => y._1 == x._1)(0)._2, nbr_parties.filter(y => y._1 == x._1)(0)._2))
-    var bests_10_many_games = moyenne_kills_many_games.sortBy(_._2, false).take(10).foreach(println)
+    var best_10_many_games = moyenne_kills_many_games.sortBy(_._2, false).take(10)
+    println("\n10 best players with more than 4 parties: \n")
+    best_10_many_games.foreach({ case (p, k, g) => {
+        println(p+" : "+k+" kills, in "+g+" parties")
+    }})
 
-*/
+    /* Résultat :
+
+    10 best players with more than 4 parties:
+
+    LawngD-a-w-n-g : 2.2 kills, in 5 parties
+    siliymaui125 : 2.0 kills, in 4 parties
+    Dcc-ccD : 1.75 kills, in 4 parties
+    dman4771 : 1.75 kills, in 4 parties
+    NerdyMoJo : 1.5 kills, in 4 parties
+    Roobydooble : 1.0 kills, in 4 parties
+    PapaNuntis : 1.0 kills, in 4 parties
+    JustTuatuatua : 0.75 kills, in 4 parties
+    ChanronG : 0.5 kills, in 4 parties
+    China_huangyong : 0.5 kills, in 4 parties
+
+    Conclusion :
+
+        Tout d'abord on observe que pour des joueurs ayant joué plus de 4 parties, le nombre de kills est très faible (1 ou 2 seulement)
+        Cela nous donne un indice sur le fait que tuer le maximum de personnes n'est peut-être pas l'objectif principal.
+        En comparant avec nos camarades, nous avons vu que les 10 meilleurs joueurs n'étaient
+        pas les mêmes si on prend en compte le nombre d'éliminations, ou si on regarde le classement final.
+        Cela signifie qu'éliminer un maximum de personne n'est pas forcément l'objectif attendu
+    */
     // ------- Exercice 2 --------
 
     // Question 1 : Fonction de score
-    val score = textFile.map(line => line.split(",")).rdd
+
+    // On commence par mapper le nom de chaque joueur avec son score lors de la partie
+    val scores = textFile.map(line => line.split(",")).rdd
         .map(fields => (fields(11), fields(5).toInt * 50 + fields(9).toInt + fields(10).toInt * 100 + 1000 - fields(14).toInt * 10))
 
-    val bests_10_score = score.sortBy(_._2, false).take(10).foreach(println)
+    /* Comme précédemment, on ne conserve que les joueur ayant effectué plus de 4 parties.
+        Pour cela on utilise la liste déjà créée auparavent many_games,
+        et on ne conserve que les joueurs qui ont leur nom dans cette liste.
+    */
+    val scores_filtered = scores.join(many_games).map { case (p, (s, k)) => (p, s) }
 
-    // Conclusion : le classement est très différent avec ce nouveau calcul de score
+    /* De même, on fait de nouveau la moyenne des différents socres obtenus
+        en faisant la somme des scores pour un joueur,
+        et en divisant par le nombre de parties effectuées
+    */
+    val sum_scores = scores_filtered.reduceByKey(_+_)
+    val mean_scores = sum_scores.map(x => (x._1, x._2 /nbr_parties.filter(y => y._1 == x._1)(0)._2))
+
+    // Enfin, on garde les 10 meilleurs et on affiche
+    val best_10_score = mean_scores.sortBy(_._2, false).take(10)
+    println("\n10 best players with score function : \n")
+    best_10_score.foreach({ case (p, s) => {
+        println(p+" has a score of "+s)
+    }})
+
+    /* Résultat :
+
+    10 best players with score function :
+
+    Dcc-ccD has a score of 1302
+    LawngD-a-w-n-g has a score of 1220
+    dman4771 has a score of 1205
+    siliymaui125 has a score of 1172
+    JustTuatuatua has a score of 1163
+    ChanronG has a score of 1117
+    NerdyMoJo has a score of 1093
+    PapaNuntis has a score of 1077
+    TemcoEwok has a score of 960
+    Informaldrip has a score of 938
+
+    Conclusion :
+
+    On retrouve finalement les mêmes éléments dans le classement avec la fonction de score que dans celui du nombre de kills,
+    la conclusion est donc que ce sont sont bien ceux qui ont tué le plus d'adversaires qui sont les mieux placés,
+    mais que le nombre de kills reste très faible.
+
+    Pour gagner, il faut donc chercher à rester en vie le plus longtemps,
+    tuer des adversaires peut rapporter des points mais n'est pas forcément nécessaire.
+    */
     spark.stop()
 }
--- TP_2/src/main/scala/Main.scala
+++ TP_2/src/main/scala/Main.scala
@@ -5,265 +5,158 @@ import scala.Console.println
 
 object app extends App {
 
-  val helper = new HelpfulFunctions()
+    val helper = new HelpfulFunctions()
 
-  val sparkConf = new SparkConf().setAppName("graphXTP").setMaster("local[1]")
-  val sc = new SparkContext(sparkConf)
+    // Création de la session Spark
+    val sparkConf = new SparkConf().setAppName("graphXTP").setMaster("local[1]")
+    val sc = new SparkContext(sparkConf)
 
-  val DATASET_PATH = "JC-202112-citibike-tripdata.csv"
-  /*  Graph creation  */
-  var trips = sc.textFile(DATASET_PATH)
-  trips = trips.filter(line => !line.contains(",,"))
+    // --------- 1. Préparation des données --------------
 
+    // Chargement du dataset
+    val DATASET_PATH = "JC-202112-citibike-tripdata.csv"
 
-  /* 1- Création de graphe */
+    //  1. Création des classes du graphes
+    var trips = sc.textFile(DATASET_PATH)
+    trips = trips.filter(line => !line.contains(",,")) // on enlève toutes les lignes où il manque des données
 
-  case class Trip(ride_id : String,rideable_type: String,started_at: Long,ended_at: Long,member_casual: String)
-  case class Station(station_id: String, station_name: String, station_latitude: Double, station_longitude: Double)
+    case class Trip(ride_id : String,rideable_type: String,started_at: Long,ended_at: Long,member_casual: String)
+    case class Station(station_id: String, station_name: String, station_latitude: Double, station_longitude: Double)
 
-val start_stations = trips.map(line => line.split(","))
-        .map(fields => (fields(4), fields(5), fields(8), fields(9)))
-val end_stations = trips.map(line => line.split(","))
-        .map(fields => (fields(6), fields(7), fields(10), fields(11)))
+    //  2. On collecte les informations des stations dans deux rdd différentes
+    val start_stations = trips.map(line => line.split(","))
+            .map(fields => (fields(4), fields(5), fields(8), fields(9)))
+    val end_stations = trips.map(line => line.split(","))
+            .map(fields => (fields(6), fields(7), fields(10), fields(11)))
 
-val stations = start_stations.union(end_stations).distinct()
+    // Puis on fait un union sur ces listes pour obtenir toutes les stations existantes
+    val stations = start_stations.union(end_stations).distinct()
 
-  //Convertissez chaque élément de l'RDD trips en objet de classe Trip.
-
-  val stationsRDD: RDD[(VertexId, Station)] = stations.zipWithIndex().map{case (line, index) =>
-    (index + 1L, Station(line._2, line._1, line._3.toDouble, line._4.toDouble))
-  }e
-
-  val stationMap: Map[String, VertexId] = stationsRDD.map{case (id, station) => (station.station_id, id)}.collect().toMap
-
-
-val tripsRDD: RDD[Edge[Trip]] = trips.map{line =>
-    val row = line split ','
-    (Edge(stationMap(row(5)), stationMap(row(7)), Trip(row(0), row(1), helper.timeToLong(row(2)), helper.timeToLong(row(3)), row(12))))
-  }
-
-  //Contruisez le graphe à partir de usersRDD et followersRDD
-
-  val graph : Graph[Station, Trip] =
-    Graph(stationsRDD, tripsRDD)
-
-  graph.vertices.collect()
-  graph.edges.collect()
-
-// 2- Calcul de degré
-//  1. subGraph
-
-graph.subgraph(trip => (trip.attr.started_at > helper.timeToLong("2021-12-05 00:00:00") && trip.attr.ended_at < helper.timeToLong("2021-12-25 00:00:00")))
-
-//  2. aggregateMessages : get number of leaving trips from each station name and associate it with the station name
-
-val leavingTrips = graph.aggregateMessages[Int](_.sendToSrc(1), _+_)
-val enteringTrips = graph.aggregateMessages[Int](_.sendToDst(1), _+_)
-
-// we map the id of the station with the name
-val leavingTripsWithNames = leavingTrips.join(graph.vertices)
-  .map { case (vertexId, (count, station)) =>
-    (station.station_name, count)
-  }
-val enteringTripsWithNames = enteringTrips.join(graph.vertices)
-  .map { case (vertexId, (count, station)) =>
-      (station.station_name, count)
-  }
-
-// sort + get top 10
-
-val top10leaving = leavingTripsWithNames.sortBy(_._2, ascending = false).take(10)
-val top10entering = enteringTripsWithNames.sortBy(_._2, ascending = false).take(10)
-
-println("Top 10 stations with the most leaving trips")
-top10leaving.foreach(println)
-
-println("Top 10 stations with the most entering trips")
-top10entering.foreach(println)
-
-// 3- Proximité entre les stations :
-//    1. Distance la plus courte
-
-val distToJC013 = graph.aggregateMessages[Double](
-triplet => {
-    if (triplet.srcAttr.station_id == "JC013" && triplet.dstAttr.station_id == "JC013") {
-        triplet.sendToDst(Double.MaxValue)
+    //  3. On convertit chaque objet Station en Vertex du graphe
+    val stationsRDD: RDD[(VertexId, Station)] = stations.zipWithIndex().map{case (line, index) =>
+        (index + 1L, Station(line._2, line._1, line._3.toDouble, line._4.toDouble))
     }
-    else if (triplet.srcAttr.station_id == "JC013") {
-        triplet.sendToDst(helper.getDistKilometers(triplet.srcAttr.station_longitude, triplet.srcAttr.station_latitude, triplet.dstAttr.station_longitude, triplet.dstAttr.station_latitude))
+
+    // On crée une map pour obtenir facilement les id des stations pour contruire les edges
+    val stationMap: Map[String, VertexId] = stationsRDD.map{case (id, station) => (station.station_id, id)}.collect().toMap
+
+    //  4. On construit les edges à l'aide des objets Trips et des id des Stations
+    val tripsRDD: RDD[Edge[Trip]] = trips.map{line =>
+        val row = line split ','
+        (Edge(stationMap(row(5)), stationMap(row(7)), Trip(row(0), row(1), helper.timeToLong(row(2)), helper.timeToLong(row(3)), row(12))))
     }
-    else if (triplet.dstAttr.station_id == "JC013") {
-        triplet.sendToSrc(helper.getDistKilometers(triplet.srcAttr.station_longitude, triplet.srcAttr.station_latitude, triplet.dstAttr.station_longitude, triplet.dstAttr.station_latitude))
+
+    //  5. On construit le graphe à l'iade de nos deux RDD Vertex et Edge
+    val graph : Graph[Station, Trip] = Graph(stationsRDD, tripsRDD)
+
+    graph.vertices.collect()
+    graph.edges.collect()
+
+    // ----------------- 2. Calcul de degré -----------------
+
+    /*  1. On extrait le sous-graphe des trajets situés entre le 5/12/2021, et le 25/12/2021
+        On utilise pour cela la fonction helper.timeToLong()
+    */
+    graph.subgraph(trip => (trip.attr.started_at > helper.timeToLong("2021-12-05 00:00:00") && trip.attr.ended_at < helper.timeToLong("2021-12-25 00:00:00")))
+
+    /*  2. On calcule le nombre de trajets sortants, et le nombre de trajets entrants pour chaque station
+        On utilise aggregateMessages en envoyant respectivement à la source et à la destination un message,
+        et en faisant la somme des messages reçus (_+_)
+    */
+    val leavingTrips = graph.aggregateMessages[Int](_.sendToSrc(1), _+_)
+    val enteringTrips = graph.aggregateMessages[Int](_.sendToDst(1), _+_)
+
+    // On associe le nom de la station à son Id pour plus de clarté
+    val leavingTripsWithNames = leavingTrips.join(graph.vertices)
+    .map { case (vertexId, (count, station)) =>
+        (station.station_name, count)
     }
-    else {
-        triplet.sendToDst(Double.MaxValue)
+    val enteringTripsWithNames = enteringTrips.join(graph.vertices)
+    .map { case (vertexId, (count, station)) =>
+        (station.station_name, count)
     }
-},
-    (a,b) => math.min(a,b)
-)
 
-val distToJC013WithNames = distToJC013.join(graph.vertices)
-  .map { case (vertexId, (count, station)) =>
-      (station.station_name, count)
-  }
+    //  3. Enfin, on trie par ordre décroissant et on prend les 10 premières stations, et on affiche
 
-distToJC013WithNames.sortBy(_._2).take(1).foreach(println)
+    val top10leaving = leavingTripsWithNames.sortBy(_._2, ascending = false).take(10)
+    val top10entering = enteringTripsWithNames.sortBy(_._2, ascending = false).take(10)
 
-//     2. Durée la plus courte
+    println("\nTop 10 stations with the most leaving trips\n")
+    top10leaving.foreach(println)
 
-val timeToJC013 = graph.aggregateMessages[Double](
-triplet => {
-    if (triplet.srcAttr.station_id == "JC013" && triplet.dstAttr.station_id == "JC013") {
-        triplet.sendToDst(Double.MaxValue)
-    }
-    else if (triplet.srcAttr.station_id == "JC013") {
-        triplet.sendToDst(triplet.attr.ended_at - triplet.attr.started_at)
-    }
-    else if (triplet.dstAttr.station_id == "JC013") {
-        triplet.sendToSrc(triplet.attr.ended_at - triplet.attr.started_at)
-    }
-    else {
-        triplet.sendToDst(Double.MaxValue)
-    }
-},
-    (a,b) => math.min(a,b)
-)
+    println("&\nTop 10 stations with the most entering trips\n")
+    top10entering.foreach(println)
 
-val timeToJC013WithNames = timeToJC013.join(graph.vertices)
-  .map { case (vertexId, (count, station)) =>
-      (station.station_name, count)
-  }
+    // ---------------- 3. Proximité entre les stations : -------------
+    //  1. Distance la plus courte
 
-timeToJC013WithNames.sortBy(_._2).take(1).foreach(println)
-
-// 4- Bonus, plus court chemin
-
-case class StationWithDist(station_id: String, station_name: String, station_latitude: Double, station_longitude: Double, distance: Double)
-
-val start_stations_with_dist = trips.map(line => line.split(","))
-        .map(fields => (fields(4), fields(5), fields(8), fields(9), Double.MaxValue))
-val end_stations_with_dist = trips.map(line => line.split(","))
-        .map(fields => (fields(6), fields(7), fields(10), fields(11), Double.MaxValue))
-
-val stations_with_dist = start_stations_with_dist.union(end_stations_with_dist).distinct()
-
-  val stations_with_distRDD: RDD[(VertexId, StationWithDist)] = stations_with_dist.zipWithIndex().map{case (line, index) =>
-    (index + 1L, StationWithDist(line._2, line._1, line._3.toDouble, line._4.toDouble, line._5))
-  }
-
-val graphWithDist : Graph[StationWithDist, Trip] =
-    Graph(stations_with_distRDD, tripsRDD)
-//val JC013distZero = graphWithDist.mapVertices((_, vd) => vd.copy(distance = if (vd.station_id == "JC013") 0 else Double.MaxValue))
-
-/*val distances = JC013distZero.pregel(Double.MaxValue, 2, EdgeDirection.Out)(
-    (id, station, newDistance) => {
-        //println("station distance = " + station.distance + ", newDistance = " + newDistance)
-        if (station.distance > newDistance) {
-            station.copy(distance = newDistance) // Mettre à jour si une distance plus courte est trouvée
-        } else {
-            station
-        }
-    },
+    val distToJC013 = graph.aggregateMessages[Double](
     triplet => {
-        val helperDist = helper.getDistKilometers(triplet.srcAttr.station_longitude, triplet.srcAttr.station_latitude, triplet.dstAttr.station_longitude, triplet.dstAttr.station_latitude)
-        val dist = triplet.srcAttr.distance + helperDist
-        //println("id = " + triplet.srcAttr.station_id + " distance = " + helperDist)
-        if(triplet.srcAttr.station_id == "JC013") {
-            println("src = " + triplet.srcAttr.station_name + ", dist = " + triplet.srcAttr.distance)
+        if (triplet.srcAttr.station_id == "JC013" && triplet.dstAttr.station_id == "JC013") {
+            triplet.sendToDst(Double.MaxValue)
         }
-        if (dist < triplet.dstAttr.distance) {
-            Iterator((triplet.dstId, dist))
+        else if (triplet.srcAttr.station_id == "JC013") {
+            triplet.sendToDst(helper.getDistKilometers(triplet.srcAttr.station_longitude, triplet.srcAttr.station_latitude, triplet.dstAttr.station_longitude, triplet.dstAttr.station_latitude))
+        }
+        else if (triplet.dstAttr.station_id == "JC013") {
+            triplet.sendToSrc(helper.getDistKilometers(triplet.srcAttr.station_longitude, triplet.srcAttr.station_latitude, triplet.dstAttr.station_longitude, triplet.dstAttr.station_latitude))
         }
         else {
-            Iterator.empty
+            triplet.sendToDst(Double.MaxValue)
         }
     },
-    (a,b) => math.min(a,b)
-)*/
+        (a,b) => math.min(a,b)
+    )
 
+    val distToJC013WithNames = distToJC013.join(graph.vertices)
+    .map { case (vertexId, (count, station)) =>
+        (station.station_name, count)
+    }
 
-val dist = Pregel(graphWithDist.mapVertices((_, vd) => vd.copy(distance = if (vd.station_id == "JC013") 0.0 else Double.MaxValue)),
-        initialMsg=Double.MaxValue, maxIterations=2,
-        activeDirection = EdgeDirection.Out) (
-        (_:VertexId, vd:StationWithDist, a:Double) => {
-            println("actual dist : " + vd.distance + ", new dist : " + a + ", min : " + math.min(a, vd.distance))
-            StationWithDist(vd.station_id, vd.station_name, vd.station_latitude, vd.station_longitude, math.min(a, vd.distance))
-        },
-        (et:EdgeTriplet[StationWithDist, Trip]) => {
-            val helperDist = helper.getDistKilometers(et.srcAttr.station_longitude, et.srcAttr.station_latitude, et.dstAttr.station_longitude, et.dstAttr.station_latitude)
-            println("helperDist : " + helperDist + " srcAttr : " + et.srcAttr.distance + " dstAttr : " + et.dstAttr.distance)
-            if (et.srcAttr.distance + helperDist < et.dstAttr.distance) {
-                Iterator((et.dstId, et.srcAttr.distance + helperDist))
-            }
-            else {
-                Iterator.empty
-            }
-        },
-        (a:Double, b:Double) => {
-            math.min(a,b)
-        })
+    distToJC013WithNames.sortBy(_._2).take(1).foreach(println)
 
-dist.vertices.foreach(
-{ case (id, station) => println(station.station_name + " is at distance " + station.distance + " from JC013")})
+    //     2. Durée la plus courte
 
-  /*  Extraction de sous-graphe
+    val timeToJC013 = graph.aggregateMessages[Double](
+    triplet => {
+        if (triplet.srcAttr.station_id == "JC013" && triplet.dstAttr.station_id == "JC013") {
+            triplet.sendToDst(Double.MaxValue)
+        }
+        else if (triplet.srcAttr.station_id == "JC013") {
+            triplet.sendToDst(triplet.attr.ended_at - triplet.attr.started_at)
+        }
+        else if (triplet.dstAttr.station_id == "JC013") {
+            triplet.sendToSrc(triplet.attr.ended_at - triplet.attr.started_at)
+        }
+        else {
+            triplet.sendToDst(Double.MaxValue)
+        }
+    },
+        (a,b) => math.min(a,b)
+    )
 
-  /*
+    val timeToJC013WithNames = timeToJC013.join(graph.vertices)
+    .map { case (vertexId, (count, station)) =>
+        (station.station_name, count)
+    }
 
-  Affichez l'entourage des noeuds Lady gaga et Barack Obama avec une solution qui fait appel
-  à la fonction \textit{subgraph}.
+    timeToJC013WithNames.sortBy(_._2).take(1).foreach(println)
 
-   */
+    // 4- Bonus, plus court chemin
 
-  val desiredUsers = List("ladygaga", "BarackObama")
+    case class StationWithDist(station_id: String, station_name: String, station_latitude: Double, station_longitude: Double, distance: Double)
 
-  toyGraph.subgraph(edgeTriplet => desiredUsers contains edgeTriplet.srcAttr.name )
-    .triplets.foreach(t => println(t.srcAttr.name + " is followed by " + t.dstAttr.name))
+    val start_stations_with_dist = trips.map(line => line.split(","))
+            .map(fields => (fields(4), fields(5), fields(8), fields(9), Double.MaxValue))
+    val end_stations_with_dist = trips.map(line => line.split(","))
+            .map(fields => (fields(6), fields(7), fields(10), fields(11), Double.MaxValue))
 
-  /*  Computation of the vertices' Degrees  */
+    val stations_with_dist = start_stations_with_dist.union(end_stations_with_dist).distinct()
 
-  /*
+    val stations_with_distRDD: RDD[(VertexId, StationWithDist)] = stations_with_dist.zipWithIndex().map{case (line, index) =>
+        (index + 1L, StationWithDist(line._2, line._1, line._3.toDouble, line._4.toDouble, line._5))
+    }
 
-  Calculez le degré (Nombre de relations sortantes) de chaque noeuds en faisant appel
-  à la méthode \texttt{aggregateMessage}.
-
-   */
-
-  val outDegreeVertexRDD = toyGraph.aggregateMessages[Int](_.sendToSrc(1), _+_)
-  outDegreeVertexRDD.collect().foreach(println)
-
-  /* Calcul de la distance du plus long chemin sortant de chaque noeud. */
-
-  /*
-
-  Créez une nouvelle classe \texttt{UserWithDistance} pour inclure la distance aux noeuds du graphe.
-
-  */
-
-  case class UserWithDistance(name: String, nickname: String, distance: Int)
-
-  /*
-  * Transformez les VD des noeuds du graphe en type UserWithDistance et initialisez la distance à 0.
-
-  * Définissez la méthode vprog (VertexId, VD, A) => VD. Cette fonction met à jour les données
-    VD du noeud ayant un identifiant VD en prenant en compte le message A
-
-  * Définissez la méthode sendMsg EdgeTriplet[VD, ED] => 	Iterator[(VertexId, A)].
-    Ayant les données des noeuds obtenues par la méthode vprog VD et des relations ED.
-    Cette fonction traite ces données pour créer un message $A$ et l'envoyer ensuite au noeuds destinataires.
-
-  * Définissez la méthode mergeMsg (A, A) => A. Cette méthode est identique à la méthode reduce
-    qui traite tous les messages reçus par un noeud et les réduit en une seule valeur.
-
-  *
-
-   */
-
-  val toyGraphWithDistance = Pregel(toyGraph.mapVertices((_, vd) => UserWithDistance(vd.name, vd.nickname, 0)),
-        initialMsg=0, maxIterations=2,
-        activeDirection = EdgeDirection.Out) (
-        (_:VertexId, vd:UserWithDistance, a:Int) => UserWithDistance(vd.name, vd.nickname, math.max(a, vd.distance)),
-        (et:EdgeTriplet[UserWithDistance, Int]) => Iterator((et.dstId, et.srcAttr.distance+1)),
-        (a:Int, b:Int) => math.max(a,b))
-  toyGraphWithDistance.vertices.foreach(user =>println(user))
-*/}
+    val graphWithDist : Graph[StationWithDist, Trip] =
+        Graph(stations_with_distRDD, tripsRDD)
+    //val JC013distZero = graphWithDist.mapVertices((_, vd) => vd.copy(distance = if (vd.station_id == "JC013") 0 else Double.MaxValue))
